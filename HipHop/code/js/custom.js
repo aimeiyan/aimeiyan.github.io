0e5185a53d30eb8b379a761767b09930ff8246b5
aimeiyan <aimeiyan@gmail.com>
2017-07-04 11:10:48 +0800

    ---

--- HipHop/code/js/custom.js
+++ HipHop/code/js/custom.js
@@ -12,5 +12,62 @@ $(function () {
         } else {
             $this.addClass("open");
         }
-    })
+    });
+
+
+    // 控制课程类型和时间安排内容的出现和消失
+    var $courseTypeOverlay = $(".course-type,.overlay");
+    var $timeArrangementOverlay = $(".time-arrangement,.overlay");
+
+    // 课程类型出现
+    $(".switch-course-type *").tap(function () {
+        $courseTypeOverlay.removeClass("slideInUp animated");
+        $courseTypeOverlay.addClass("slideInDown animated");
+    });
+
+    // 课程类型消失
+    $(".course-type").tap(function () {
+        $courseTypeOverlay.removeClass("slideInDown animated");
+        $courseTypeOverlay.addClass("slideInUp animated");
+    });
+
+
+    // 时间安排出现
+    $(".switch-time-arrangement *").tap(function () {
+        $timeArrangementOverlay.removeClass("slideInUp animated");
+        $timeArrangementOverlay.addClass("slideInDown animated");
+    });
+
+    // 时间安排消失
+    $(".time-arrangement").tap(function () {
+        $timeArrangementOverlay.removeClass("slideInDown animated");
+        $timeArrangementOverlay.addClass("slideInUp animated");
+    });
+
+    // 时间安排页面选项点击高亮
+    // 上课日期
+    $(".course-date-list a").tap(function (e) {
+        event.stopPropagation();
+        $(".course-time-list li").removeClass("active");  //去掉时间安排选项
+
+        var $this = $(this);
+        var $parent = $this.parent();
+
+        if ($parent.hasClass("active")) {
+            $this.parent().removeClass("active");
+        } else {
+            $this.parent().addClass("active");
+        }
+
+    });
+
+    //授课时间
+    $(".course-time-list a").tap(function (e) {
+        event.stopPropagation();
+        $(".course-date-list .course-date-text-wrp").removeClass("active");  //去掉上课日期选项
+        var $this = $(this);
+        $this.parent().siblings().removeClass("active");
+        $this.parent().addClass("active");
+    });
+
 });
